b50c2daf896cafa048ef90306baa3bf89889ed67
wangqiang1122 <wangqiangle@163.com>
2019-12-11 12:07:09 +0800

    falt初级

--- rnDay1/index.js
+++ rnDay1/index.js
@@ -9,4 +9,4 @@ import index3 from './js/page/input'
 import webView from './js/page/webview'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => webView);
+AppRegistry.registerComponent(appName, () => FlatList);
