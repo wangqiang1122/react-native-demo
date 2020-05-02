602b6caa8d20456142f5cc56fb456e538448578b
admin <admin@admindeMacBook-Pro.local>
2020-05-02 10:33:12 +0800

    更新

--- day1/App.js
+++ day1/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,SwipeableFlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View,SwipeableFlatList,ScrollView, SafeAreaView} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,13 +18,69 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+    this.state={
+        name: "袋子 i",
+    }
+  }
+  bianhua=() => {
+    // this.setState({
+    //     name: '我变了'
+    // })
+      this.state.name='变了'
+  }
+    onScrollEndDrag = (e)=>{
+      console.log(e)
+    }
   render() {
+    this.bianhua();
+    let TimeArr = [];
+      for (let a = 0;a<24;a++) {
+          if (a>10) {
+              TimeArr.push(`${a}:00`)
+
+          } else{
+              TimeArr.push(`0${a}:00`)
+          }
+      }
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+
+            <View style={styles.container}>
+                <ScrollView
+                    onScrollEndDrag={this.onScrollEndDrag}
+                    horizontal = {true}
+                >
+                    <View style={styles.view1}>
+                        <Text>{this.state.name}</Text>
+
+                    </View>
+                    <View style={styles.view1}>
+                        <Text>dada</Text>
+
+                    </View>
+                    <View style={styles.view1}>
+                        <Text>dada</Text>
+                    </View>
+                    <View style={styles.view1}>
+                        <Text>dada</Text>
+                    </View>
+                    <View style={styles.view1}>
+                        <Text>dada</Text>
+
+                    </View>
+                    <View style={styles.view1}>
+                        <Text>dada</Text>
+
+                    </View>
+                </ScrollView>
+                <Text style={styles.welcome}>Welcome to React Native!S</Text>
+                <Text style={styles.instructions}>To get started, edit App.js发发</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+                <Text>{ JSON.stringify(TimeArr)}</Text>
+            </View>
+        </SafeAreaView>
     );
   }
 }
@@ -45,5 +101,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+   view1: {
+       width:200,
+       height: 200,
+       display: 'flex',
+       justifyContent: 'center',
+       alignItems: 'center',
+       color: 'blue',
+       borderColor: 'red',
+       borderWidth: 1
   },
 });
